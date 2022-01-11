1f77ae7a7fb076b9410da1ea5b9b7182bc4e7744
Francisco Marcelo Mendes Damasceno <franciscodamasceno@mpam.mp.br>
2022-01-11 12:39:19 -0400

    fixed: load dataset path

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import nltk
+from app import app
 
-data = pd.read_csv("datasets/dataset.csv", dtype={'ano': 'object'})
+data = pd.read_csv(app.server.root_path+"/datasets/dataset.csv", dtype={'ano': 'object'})
 data['data'] = pd.to_datetime(data.ano, format='%Y')
 
 stopwords = nltk.corpus.stopwords.words()
 
 if __name__ == '__main__':
-    nltk.download('stopwords')
+    pass
